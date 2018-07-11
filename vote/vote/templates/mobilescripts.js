99fb168e818c9e667c9674105469b4693ed9fa60
CanSevgi <hcansevgii@gmail.com>
2018-07-11 12:02:27 +0300

    Clean Up
    Cleaned Junk Files

---
+++ vote/vote/templates/mobilescripts.js
@@ -0,0 +1,14 @@
+// This file did not used!
+
+if (screen.width >= 700) {
+    document.location = "/";
+    }
+
+    function resetdivopen() {
+        var x = document.getElementById("resetdiv");
+        if (x.style.display == "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    }
---
+++ vote/vote/templates/scripts.js
@@ -0,0 +1,13 @@
+// This file did not used!
+if (screen.width <= 699) {
+    document.location = "mobile";
+    }
+
+    function resetdivopen() {
+        var x = document.getElementById("resetdiv");
+        if (x.style.display == "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    }
